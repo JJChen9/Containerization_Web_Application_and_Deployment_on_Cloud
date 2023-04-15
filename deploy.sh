cc2ecab828f6fa341eab8aa99e2e37faf960a543
JIJUN CHEN <75265889+jijunc@users.noreply.github.com>
2023-04-14 21:17:42 -0400

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t michaelzhu101/multi-client:latest -t michaelzhu101/multi-client:$SHA ./client
+docker build -t michaelzhu101/multi-server:latest -t michaelzhu101/multi-server:$SHA ./server
+docker build -t michaelzhu101/multi-worker:latest -t michaelzhu101/multi-worker:$SHA ./worker
+# Log in to the docker CLI
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
+# Take those images and push them to docker hub
+docker push michaelzhu101/multi-client:latest
+docker push michaelzhu101/multi-server:latest
+docker push michaelzhu101/multi-worker:latest
+docker push michaelzhu101/multi-client:$SHA
+docker push michaelzhu101/multi-server:$SHA
+docker push michaelzhu101/multi-worker:$SHA
+#  apply all configs in the k8s folder
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=michaelzhu101/multi-server:$SHA
+kubectl set image deployments/client-deployment client=michaelzhu101/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=michaelzhu101/multi-worker:$SHA
